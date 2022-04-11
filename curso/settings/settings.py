c55d2acee3a26cfe8c059b3ecb93fd4c0f449036
Douglas Gabriel <douglaasg11@gmail.com>
2022-04-10 22:56:47 -0300

    Correção

--- curso/settings/settings.py
+++ curso/settings/settings.py
@@ -57,9 +57,7 @@ ROOT_URLCONF = 'curso.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates_base'),
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- receitas/views.py
+++ receitas/views.py
@@ -3,12 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'receitas/paginas/home.html', context={
-        'nome': 'Nome da Receita',
-    })
+    return render(request, 'receitas/paginas/home.html')
 
 
 def receitas(request, id):
-    return render(request, 'receitas/paginas/view-receitas.html', context={
-        'nome': 'Nome da Receita',
-    })
+    return render(request, 'receitas/paginas/view-receitas.html')
